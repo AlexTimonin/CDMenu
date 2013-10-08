3fb78c1526b4a34bf77f825a84f78a107acaa73a
Andrew Cherkashin <andrew@bekitzur.com>
2013-10-07 18:42:48 -0700

    Added: createDialogMenu method

--- library/src/com/bekitzur/cdmenu/CDMenu.java
+++ library/src/com/bekitzur/cdmenu/CDMenu.java
@@ -1,7 +1,27 @@
 package com.bekitzur.cdmenu;
 
+import android.content.Context;
+
 /**
- * Created by andreycherkashin on Oct/7/13.
+ * Customizable Dialog Menu for Android
+ *    https://github.com/BeKitzur/CDMenu
  */
 public class CDMenu {
+
+    private Context context;
+
+    /**
+     * Creates new CDMenu
+     * @param context context which CDMenu will use to show itself
+     * @return newly created CDMenu
+     * @throws IllegalArgumentException on context == 'null'
+     */
+    public static CDMenu createDialogMenu(Context context) {
+        if (context == null) {
+            throw new IllegalArgumentException("Null context where passed into createDialogMenu method");
+        }
+        CDMenu cDMenu = new CDMenu();
+        cDMenu.context = context;
+        return cDMenu;
+    }
 }
