ccec977e9e2fc7b2064b5d82b9c85f0e8ce14635
Alexander Timonin <lxtimonin@gmail.com>
2013-10-08 10:47:30 +0300

    setCustomListView, setCustomListItem, setOnItemClickListener methods

---
+++ library/src/com/bekitzur/cdmenu/StyleListener.java
@@ -0,0 +1,10 @@
+package com.bekitzur.cdmenu;
+
+import android.view.View;
+
+/**
+ * created by alext on Oct/07/13.
+ */
+public interface StyleListener {
+    void onStyleChangeRequested(View listItemView, int position);
+}
